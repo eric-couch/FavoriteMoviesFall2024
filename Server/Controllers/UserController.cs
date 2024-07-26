89911c70eb570d1c5d77e329f3fd2fc31eebc150
Eric Couch <ericcouch@yahoo.com>
2024-07-26 13:25:33 -0500

    added user id to controller method

--- Server/Controllers/UserController.cs
+++ Server/Controllers/UserController.cs
@@ -1,5 +1,8 @@
 ﻿using FavoriteMoviesFall2024.Server.Data;
+using FavoriteMoviesFall2024.Server.Models;
 using FavoriteMoviesFall2024.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,23 +11,35 @@ namespace FavoriteMoviesFall2024.Server.Controllers;
 public class UserController : Controller
 {
     private readonly ApplicationDbContext _context;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public UserController(ApplicationDbContext context)
+    public UserController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
     {
         _context = context;
+        _userManager = userManager;
     }
 
     [HttpGet]
     [Route("api/User")]
-    public async Task<UserDto> GetMovies()
+    public async Task<IActionResult> GetMovies(string userId)
     {
-        var user = await _context.Users.Include(u => u.FavoriteMovies)
+        if (userId is not null)
+        {
+            var user = await _userManager.FindByNameAsync(userId);
+
+            var movies = await _context.Users.Include(u => u.FavoriteMovies)
                             .Select(u => new UserDto
                             {
                                 Id = u.Id,
                                 FavoriteMovies = u.FavoriteMovies
-                            }).FirstOrDefaultAsync();
+                            })
+                            .FirstOrDefaultAsync(u => u.Id == user.Id);
 
-        return user;
+            return Ok(user);
+        } else
+        {
+            return NotFound();
+        }
+        
     }
 }
