7bafbdfeda509e7c008c761fd2c35252d2b1d73f
Eric Couch <ericcouch@yahoo.com>
2024-08-26 13:00:43 -0500

    added server side caching for OMDBMovie objects in the userService getmovies method

--- Server/Services/UserService.cs
+++ Server/Services/UserService.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using FavoriteMoviesFall2024.Shared.Wrapper;
 using System.Diagnostics;
+using FavoriteMoviesFall2024.Server.Migrations;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace FavoriteMoviesFall2024.Server.Services;
 
@@ -15,22 +17,26 @@ public class UserService : IUserService
     private readonly RoleManager<IdentityRole> _roleManager;
     private readonly ILogger<UserService> _logger;
     private readonly HttpClient _httpClient;
+    private readonly IMemoryCache _cache;
 
     public UserService( ApplicationDbContext context,
                         RoleManager<IdentityRole> roleManager,
                         UserManager<ApplicationUser> userManager,
                         ILogger<UserService> logger,
-                        HttpClient httpClient)
+                        HttpClient httpClient,
+                        IMemoryCache cache)
     {
         _context = context;
         _roleManager = roleManager;
         _userManager = userManager;
         _logger = logger;
         _httpClient = httpClient;
+        _cache = cache;
     }
 
     private readonly string OMDBAPIUrl = "https://www.omdbapi.com/?";
     private readonly string OMDBAPIKey = "apikey=86c39163";
+    private readonly string cacheKey = "OMDBMovies";
 
     public async Task<UserDto> GetMovies(string userName)
     {
@@ -43,46 +49,72 @@ public class UserService : IUserService
                 return new UserDto();
             }
 
-            // get user's info and favorite movies
-            var movies = await _context.Users.Include(u => u.FavoriteMovies)
-                            .Select(u => new UserDto
-                            {
-                                Id = u.Id,
-                                UserName = u.UserName,
-                                FavoriteMovies = u.FavoriteMovies
-                            })
-                            .FirstOrDefaultAsync(u => u.Id == user.Id);
+            UserDto? movies = new();
 
-            // get the movies that aren't the cache yet and add them to the cache
-            var missingMovies = (from m in movies.FavoriteMovies
-                                    where !_context.OMDBMovies.Any(omdb => omdb.imdbID == m.imdbId)
-                                    select m).ToList();
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
 
-            // add omdb movie cache for all missing movies
-            foreach (var missingMovie in missingMovies)
+            if (_cache.TryGetValue(cacheKey, out IEnumerable<OMDBMovie> _movies)) {
+                _logger.LogInformation("Retrieved movies from cache");
+                movies.OMDBMovies = _movies.ToList();
+            } else
             {
-                OMDBMovie omdbMovie = await _httpClient.GetFromJsonAsync<OMDBMovie>($"{OMDBAPIUrl}{OMDBAPIKey}&i={missingMovie.imdbId}");
-                if (omdbMovie is not null)
+                // get user's info and favorite movies
+                movies = await _context.Users.Include(u => u.FavoriteMovies)
+                                .Select(u => new UserDto
+                                {
+                                    Id = u.Id,
+                                    UserName = u.UserName,
+                                    FavoriteMovies = u.FavoriteMovies
+                                })
+                                .FirstOrDefaultAsync(u => u.Id == user.Id);
+
+                // get the movies that aren't the cache yet and add them to the cache
+                var missingMovies = (from m in movies.FavoriteMovies
+                                     where !_context.OMDBMovies.Any(omdb => omdb.imdbID == m.imdbId)
+                                     select m).ToList();
+
+                // add omdb movie cache for all missing movies
+                foreach (var missingMovie in missingMovies)
                 {
-                    await _context.Ratings.AddRangeAsync(omdbMovie.Ratings);
-                    //omdbMovie.Ratings = await _context.Ratings.Where(r => r.OMDBMovieId == omdbMovie.imdbID).ToListAsync();
-                    await _context.OMDBMovies.AddAsync(omdbMovie);
-                    await _context.SaveChangesAsync();
+                    OMDBMovie omdbMovie = await _httpClient.GetFromJsonAsync<OMDBMovie>($"{OMDBAPIUrl}{OMDBAPIKey}&i={missingMovie.imdbId}");
+                    if (omdbMovie is not null)
+                    {
+                        await _context.Ratings.AddRangeAsync(omdbMovie.Ratings);
+                        await _context.OMDBMovies.AddAsync(omdbMovie);
+                        await _context.SaveChangesAsync();
+                    }
                 }
+
+                // grab all the omdb movies for the user and add it to the user dto
+                var omdbMoviesForUser = await (from omdb in _context.OMDBMovies
+                                               join m in _context.Movies on omdb.imdbID equals m.imdbId
+                                               join u in _context.Users on m.ApplicationUserId equals u.Id
+                                               where m.ApplicationUserId == user.Id
+                                               select omdb)
+                                               .Include(omdb => omdb.Ratings)
+                                               .ToListAsync();
+
+                movies.OMDBMovies = omdbMoviesForUser;
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromSeconds(60))
+                    .SetAbsoluteExpiration(TimeSpan.FromMinutes(60))
+                    .SetPriority(CacheItemPriority.Normal);
+
+                // add movies to cache
+                _cache.Set(cacheKey, omdbMoviesForUser, cacheEntryOptions);
+
             }
 
-            // grab all the omdb movies for the user and add it to the user dto
-            //var omdbMoviesForUser = (from omdb in _context.OMDBMovies
-            //                         join m in movies.FavoriteMovies on omdb.imdbID equals m.imdbId
-            //                         select omdb).ToList();
+            
 
-            var omdbMoviesForUser = await (from omdb in _context.OMDBMovies
-                                           join m in _context.Movies on omdb.imdbID equals m.imdbId
-                                           join u in _context.Users on m.ApplicationUserId equals u.Id
-                                           where m.ApplicationUserId == user.Id
-                                           select omdb).ToListAsync();
-
-            movies.OMDBMovies = omdbMoviesForUser;
+            stopwatch.Stop();
+            _logger.LogInformation("Elapsed Time retrieving movies: {ElapsedMilliseconds} ms", stopwatch.ElapsedMilliseconds);
+            // api calls took       Elapsed Time retrieving movies: 1718 ms
+            // from db took         Elapsed Time retrieving movies: 1219 ms
+            // from db second pass  Elapsed Time retrieving movies: 1262 ms
+            // from cache           Elapsed Time retrieving movies: 2 ms    
 
             return movies;
         } catch (Exception e)
